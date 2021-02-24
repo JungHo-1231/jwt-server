32780bc2fc69964f23827521749d45a6de2da150
jung <jungpark007@naver.com>
2021-02-24 23:40:42 +0900

    24강

--- src/main/java/com/example/jwtserver/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/example/jwtserver/config/auth/PrincipalDetailsService.java
@@ -1,19 +1,26 @@
 package com.example.jwtserver.config.auth;
 
+import com.example.jwtserver.model.User;
+import com.example.jwtserver.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+// http://localhost:8080/login
 @Service
 @RequiredArgsConstructor
 public class PrincipalDetailsService implements UserDetailsService {
 
-//    private final
+    private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        System.out.println("PrincipalDetailsService 의 loadUserByUsername()");
+        User userEntity = userRepository.findByUserName(userName);
+        return new PrincipalDetails(userEntity);
     }
 }
